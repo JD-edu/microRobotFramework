6099825307e0d4f7cfcf50dcb9ab8992a1bba08c
JD-edu <96219601+JD-edu@users.noreply.github.com>
2025-07-15 22:00:24 +0900

    July 15th

--- microRobotFramework_01/mpu6050_get_raw_data/mpu6050_get_raw_data.ino
+++ microRobotFramework_01/mpu6050_get_raw_data/mpu6050_get_raw_data.ino
@@ -6,32 +6,15 @@ MPU6050 mpu(Wire);
 
 long timer = 0;
 
-/*
-packet 
-header 0xf5
-length
-accelX 0
-accelX 1
-accelY 0
-accelY 1
-accelZ 0
-accelZ 1  
-gyroX 0
-gyroX 1
-gyroY 0
-gyroY 1
-gyroZ 0
-gyroZ 1  
-checksum
-*/
-byte data[23]; 
+// packet structure: header (1byte) + 10 * int16_t (20bytes) = 21 bytes total
+// length and checksum fields are not explicitly sent, simplifying the protocol
+byte dataBuffer[21]; // Total bytes to send: 1 (header) + 20 (data) = 21 bytes
 
 void setup(){
   Serial.begin(115200);
   Wire.begin();
 
   byte status = mpu.begin();
-  //Serial.print("MPU6050 status: ");
   Serial.println(status);
   pinMode(13, OUTPUT);
 
@@ -40,7 +23,7 @@ void setup(){
     delay(100);
     digitalWrite(13, LOW);
     delay(100);
-    
+    status = mpu.begin(); // 재시도 로직 추가 (선택 사항)
   }
 
   Serial.println("Calculation offset. Do not move");
@@ -56,43 +39,48 @@ uint16_t encoder4 = 0;
 
 void loop() {
   mpu.update();
-  if(millis() - timer > 50){
-    //Serial.println(mpu.getAccX());
+  if(millis() - timer > 50){ // 50ms마다 데이터 전송
     int16_t accelX = (int16_t)(mpu.getAccX()*1000);
     int16_t accelY = (int16_t)(mpu.getAccY()*1000);
     int16_t accelZ = (int16_t)(mpu.getAccZ()*1000);
     int16_t gyroX = (int16_t)(mpu.getGyroX()*1000);
     int16_t gyroY = (int16_t)(mpu.getGyroY()*1000);
     int16_t gyroZ = (int16_t)(mpu.getGyroZ()*1000);
-    data[0] = 0xf5;
-    data[1] = 21;
-    data[2] = (accelX >> 8) & 0xff;
-    data[3] = accelX & 0xff;
-    data[4] = (accelY >> 8) & 0xff;
-    data[5] = accelY & 0xff;
-    data[6] = (accelZ >> 8) & 0xff;
-    data[7] = accelZ & 0xff;
-    data[8] = (gyroX >> 8) & 0xff;
-    data[9] = gyroX & 0xff;
-    data[10] = (gyroY >> 8) & 0xff;
-    data[11] = gyroY & 0xff;
-    data[12] = (gyroZ >> 8) & 0xff;
-    data[13] = gyroZ & 0xff;
-    data[14] = (encoder1 >> 8) & 0xff;
-    data[15] = encoder1 & 0xff;
-    data[16] = (encoder2 >> 8) & 0xff;
-    data[17] = encoder2 & 0xff;
-    data[18] = (encoder3 >> 8) & 0xff;
-    data[19] = encoder3 & 0xff;
-    data[20] = (encoder4 >> 8) & 0xff;
-    data[21] = encoder4 & 0xff;
-    data[22] = 0;
-    Serial.write(data, 23);
+
+    // Header byte
+    dataBuffer[0] = 0xF5; // Header
+
+    // MPU6050 data (6 * int16_t = 12 bytes) - Big-Endian
+    dataBuffer[1] = (accelX >> 8) & 0xFF;
+    dataBuffer[2] = accelX & 0xFF;
+    dataBuffer[3] = (accelY >> 8) & 0xFF;
+    dataBuffer[4] = accelY & 0xFF;
+    dataBuffer[5] = (accelZ >> 8) & 0xFF;
+    dataBuffer[6] = accelZ & 0xFF;
+    dataBuffer[7] = (gyroX >> 8) & 0xFF;
+    dataBuffer[8] = gyroX & 0xFF;
+    dataBuffer[9] = (gyroY >> 8) & 0xFF;
+    dataBuffer[10] = gyroY & 0xFF;
+    dataBuffer[11] = (gyroZ >> 8) & 0xFF;
+    dataBuffer[12] = gyroZ & 0xFF;
+
+    // Encoder data (4 * uint16_t = 8 bytes) - Big-Endian
+    dataBuffer[13] = (encoder1 >> 8) & 0xFF;
+    dataBuffer[14] = encoder1 & 0xFF;
+    dataBuffer[15] = (encoder2 >> 8) & 0xFF;
+    dataBuffer[16] = encoder2 & 0xFF;
+    dataBuffer[17] = (encoder3 >> 8) & 0xFF;
+    dataBuffer[18] = encoder3 & 0xFF;
+    dataBuffer[19] = (encoder4 >> 8) & 0xFF;
+    dataBuffer[20] = encoder4 & 0xFF;
+    
+    // Total 1 (header) + 20 (data) = 21 bytes sent
+    Serial.write(dataBuffer, 21); // Send 21 bytes
+
     encoder1++; 
     encoder2++;
     encoder3++;
     encoder4++;
     timer = millis();
   }
-
 }
